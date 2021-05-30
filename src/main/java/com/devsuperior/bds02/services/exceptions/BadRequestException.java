26917509725a7b0d83e526e83e4fc12900c18b82
Tarciso Torres <tarcisoftorres@gmail.com>
2021-05-30 17:30:58 -0300

    test: finalize tests

---
+++ src/main/java/com/devsuperior/bds02/services/exceptions/BadRequestException.java
@@ -0,0 +1,11 @@
+package com.devsuperior.bds02.services.exceptions;
+
+public class BadRequestException  extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public BadRequestException(String msg) {
+		super(msg);
+	}
+
+}
